c82b207637644b44191cedd38d54ecb1436cd291
Michael Harris <mhguitar1997@gmail.com>
2019-07-18 12:44:10 -0700

    Add route that will render login component

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import {Route} from 'react-router-dom'
-import './App.css';
+import React from "react";
+import { Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-  
+      <Route path="/" component={Login} />
     </div>
   );
 }
