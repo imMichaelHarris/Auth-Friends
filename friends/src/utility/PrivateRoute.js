df49d46743dec686ac46732764d93891977509b0
Michael Harris <mhguitar1997@gmail.com>
2019-07-18 14:27:23 -0700

    Import Redirect and Route fro react-router-dom

---
+++ friends/src/utility/PrivateRoute.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Redirect, Route} from 'react-router-dom'
+
+const PrivateRoute = ({}) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default PrivateRoute;
