91c8961f627fa0451bd2e1d52730adcd3d003b47
Michael Harris <mhguitar1997@gmail.com>
2019-07-18 14:32:30 -0700

    Create private route HOC

--- friends/src/utility/PrivateRoute.js
+++ friends/src/utility/PrivateRoute.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Redirect, Route} from 'react-router-dom'
 
-const PrivateRoute = ({}) => {
-    return (
-        <div>
-            
-        </div>
-    );
+const PrivateRoute = ({component: Component}, ...rest) => {
+    if(localStorage.getItem('token')){
+        return <Route {...rest} render={props => <Component {...props} />} />
+    } else {
+        return <Redirect to="/" />
+    }
 };
 
 export default PrivateRoute;
