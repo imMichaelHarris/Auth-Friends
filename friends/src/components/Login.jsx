f732feac9e0b159dedc305451189a0cd196d5033
Michael Harris <mhguitar1997@gmail.com>
2019-07-18 12:53:48 -0700

    Basic form add for login component

--- friends/src/components/Login.jsx
+++ friends/src/components/Login.jsx
@@ -5,8 +5,22 @@ const Login = () => {
     return (
         <div>
             Login
+            <Form>
+                <legend>Logn</legend>
+                <label>Email</label>
+                <Field name="email" type="email" placeholder="Email" />
+                <label>Password</label>
+                <Field name="password" type="password" placeholder="Password" />
+            </Form>
         </div>
     );
 };
 
-export default Login;
+export default withFormik({
+    mapPropsToValues({email, password}) {
+        return {
+            email: email || "",
+            password: password || ""
+        }
+    }
+})(Login);
