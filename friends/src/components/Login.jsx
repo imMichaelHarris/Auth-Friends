3a3371006d18a1c4a16361a3dd5f12d483e4b9a2
Michael Harris <mhguitar1997@gmail.com>
2019-07-18 13:00:50 -0700

    Import axiosWithAuth in Login component

--- friends/src/components/Login.jsx
+++ friends/src/components/Login.jsx
@@ -1,30 +1,31 @@
-import React from 'react';
-import {withFormik, Form, Field} from 'formik'
+import React from "react";
+import { withFormik, Form, Field } from "formik";
+import { axiosWithAuth } from "../utility/axiosWithAuth";
 
 const Login = () => {
-    return (
-        <div>
-            Login
-            <Form>
-                <legend>Logn</legend>
-                <label>Email</label>
-                <Field name="email" type="email" placeholder="Email" />
-                <label>Password</label>
-                <Field name="password" type="password" placeholder="Password" />
-                <button>Login</button>
-            </Form>
-        </div>
-    );
+  return (
+    <div>
+      Login
+      <Form>
+        <legend>Logn</legend>
+        <label>Email</label>
+        <Field name="email" type="email" placeholder="Email" />
+        <label>Password</label>
+        <Field name="password" type="password" placeholder="Password" />
+        <button>Login</button>
+      </Form>
+    </div>
+  );
 };
 
 export default withFormik({
-    mapPropsToValues({email, password}) {
-        return {
-            email: email || "",
-            password: password || ""
-        }
-    },
-    handleSubmit(values){
-        console.log(values)
-    }
+  mapPropsToValues({ email, password }) {
+    return {
+      email: email || "",
+      password: password || ""
+    };
+  },
+  handleSubmit(values) {
+    console.log(values);
+  }
 })(Login);
